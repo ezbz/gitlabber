38a63e2921f2c08fc7cc07153eac921548d9a6df
Erez Mazor <erezmazor@gmail.com>
2020-05-11 18:17:04 +0300

    revert unwated changes from pull request

--- gitlabber/git.py
+++ gitlabber/git.py
@@ -3,45 +3,20 @@ import os
 import sys
 import subprocess
 import git
-import concurrent.futures
+
 log = logging.getLogger(__name__)
 
 
-class Action:
-    def __init__(self, node, path):
-        self.node = node
-        self.path = path
-
-    def __repr__(self):
-        return "<Action n:%s p:%s>" % (self.node, self.path)
-
-def collect_actions(root, dest):
-    actions = []
+def sync_tree(root, dest):
     for child in root.children:
         path = "%s%s" % (dest, child.root_path)
         if not os.path.exists(path):
             os.makedirs(path)
         if child.is_leaf:
-            actions.append(Action(node=child, path=path))
-            # clone_or_pull_project(child, path)
+            clone_or_pull_project(child, path)
         if not child.is_leaf:
-            actions.extend(collect_actions(child, dest))
-    return actions
+            sync_tree(child, dest)
 
-
-def sync_tree(root, dest):
-    actions = collect_actions(root, dest)
-    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
-        executor.map(clone_or_pull_project, actions)
-# def sync_tree(root, dest):
-#     for child in root.children:
-#         path = "%s%s" % (dest, child.root_path)
-#         if not os.path.exists(path):        
-#             os.makedirs(path)
-#         if child.is_leaf:
-#             clone_or_pull_project(child, path)
-#         if not child.is_leaf:
-#             sync_tree(child, dest)
 
 def is_git_repo(path):
     try:
@@ -50,9 +25,8 @@ def is_git_repo(path):
     except git.InvalidGitRepositoryError:
         return False
 
-def clone_or_pull_project(action):
-    node = action.node
-    path = action.path
+
+def clone_or_pull_project(node, path):
     if is_git_repo(path):
         '''
         Update existing project
@@ -76,10 +50,8 @@ def clone_or_pull_project(action):
             git.Repo.clone_from(node.url, path)
         except KeyboardInterrupt:
             log.error("User interrupted")
-            sys.exit(0)        
+            sys.exit(0)
         except Exception as e:
             log.error("Error cloning project %s", path)
             log.error(e)
-
-
 
--- gitlabber/gitlab_tree.py
+++ gitlabber/gitlab_tree.py
@@ -12,6 +12,7 @@ import logging
 import os
 log = logging.getLogger(__name__)
 
+
 class GitlabTree:
 
     def __init__(self, url, token, includes=[], excludes=[], in_file=None):
@@ -28,7 +29,6 @@ class GitlabTree:
             self.progress = tqdm(total=total, unit="projects",
                                  bar_format="{desc}: {percentage:.1f}%|{bar:100}| {n_fmt}/{total_fmt}{postfix}")
 
-
     def update_progress_length(self, added):
         if self.progress is not None:
             self.progress.total = self.progress.total + added
@@ -37,12 +37,11 @@ class GitlabTree:
     def show_progress(self, text):
         if self.progress is not None:
             self.progress.update(1)
-            self.progress.set_postfix({'gitlab':text})
+            self.progress.set_postfix({'gitlab': text})
 
     def finish_progress(self):
         if self.progress is not None:
             self.progress.close()
-
 
     def is_included(self, node):
         '''
@@ -51,12 +50,12 @@ class GitlabTree:
         if self.includes is not None:
             for include in self.includes:
                 if globre.match(include, node.root_path):
-                    log.debug("Matched include path [%s] to node [%s]", include, node.root_path)
+                    log.debug(
+                        "Matched include path [%s] to node [%s]", include, node.root_path)
                     return True
         else:
             return True
-        
-                    
+
     def is_excluded(self, node):
         '''
         returns True if the node should be excluded 
@@ -64,7 +63,8 @@ class GitlabTree:
         if self.excludes is not None:
             for exclude in self.excludes:
                 if globre.match(exclude, node.root_path):
-                    log.debug("Matched exclude path [%s] to node [%s]", exclude, node.root_path)
+                    log.debug(
+                        "Matched exclude path [%s] to node [%s]", exclude, node.root_path)
                     return True
         return False
 
@@ -80,30 +80,32 @@ class GitlabTree:
         return "/".join([str(n.name) for n in node.path])
 
     def make_node(self, name, parent, url):
-        node= Node(name=name, parent=parent, url=url)
+        node = Node(name=name, parent=parent, url=url)
         node.root_path = self.root_path(node)
         return node
 
     def get_projects(self, group, parent):
-        projects= group.projects.list(as_list=False)
+        projects = group.projects.list(as_list=False)
         self.update_progress_length(len(projects))
         for project in projects:
-            node = self.make_node(project.name, parent, url=project.ssh_url_to_repo)
+            node = self.make_node(project.name, parent,
+                                  url=project.ssh_url_to_repo)
             self.show_progress(node.name)
 
     def get_subgroups(self, group, parent):
-        subgroups= group.subgroups.list(as_list=False)
+        subgroups = group.subgroups.list(as_list=False)
         self.update_progress_length(len(subgroups))
         for subgroup_def in subgroups:
-            subgroup= self.gitlab.groups.get(subgroup_def.id)
+            subgroup = self.gitlab.groups.get(subgroup_def.id)
             node = self.make_node(subgroup.name, parent, url=subgroup.web_url)
             self.show_progress(node.name)
             self.get_subgroups(subgroup, node)
             self.get_projects(subgroup, node)
 
     def load_gitlab_tree(self):
-        log.info("Fetching group/project tree structure from Gitlab at [%s]", self.url)
-        groups= self.gitlab.groups.list(as_list=False)
+        log.info(
+            "Fetching group/project tree structure from Gitlab at [%s]", self.url)
+        groups = self.gitlab.groups.list(as_list=False)
         self.init_progress(len(groups))
         for group in groups:
             node = self.make_node(group.name, self.root, url=group.web_url)
@@ -156,9 +158,8 @@ class GitlabTree:
 
     def sync_tree(self, dest):
         log.debug("Going to clone/pull [%s] groups and [%s] projects" %
-              (len(self.root.descendants) - len(self.root.leaves), len(self.root.leaves)))
+                  (len(self.root.descendants) - len(self.root.leaves), len(self.root.leaves)))
         sync_tree(self.root, dest)
 
     def is_empty(self):
         return self.root.height < 1
-
