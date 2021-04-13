5ee5ea82943e5b4c52ddab33770411b6b7f178f4
Gabriel Augendre <gabriel@augendre.info>
2021-04-13 15:07:26 +0200

    Fix elapsed time computation (#70)

--- gitlabber/progress.py
+++ gitlabber/progress.py
@@ -29,7 +29,7 @@ class ProgressBar:
     def finish_progress(self):
         if self.progress is not None:
             self.progress.close()
-            end = time.time() - self.start
+            end = time.time()
             hours, rem = divmod(end-self.start, 3600)
             minutes, seconds = divmod(rem, 60)
             return "{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds)
