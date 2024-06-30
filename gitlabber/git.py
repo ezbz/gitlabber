12cb9218520ac5bcb2cba021728f1e16ce9a52cf
Erez <erez@dots.eco>
2024-06-30 22:49:15 +0700

    fix parameter naming

--- gitlabber/git.py
+++ gitlabber/git.py
@@ -12,33 +12,33 @@ progress = ProgressBar('* syncing projects')
 
 
 class GitAction:
-    def __init__(self, node, path, recursive=False, use_fetch=False, inline_token=False):
+    def __init__(self, node, path, recursive=False, use_fetch=False, hide_token=False):
         self.node = node
         self.path = path
         self.recursive = recursive
         self.use_fetch = use_fetch
-        self.inline_token = inline_token
+        self.hide_token = hide_token
 
-def sync_tree(root, dest, concurrency=1, disable_progress=False, recursive=False, use_fetch=False, inline_token=False):
+def sync_tree(root, dest, concurrency=1, disable_progress=False, recursive=False, use_fetch=False, hide_token=False):
     if not disable_progress:
         progress.init_progress(len(root.leaves))
-    actions = get_git_actions(root, dest, recursive, use_fetch, inline_token)
+    actions = get_git_actions(root, dest, recursive, use_fetch, hide_token)
     with concurrent.futures.ThreadPoolExecutor(max_workers=concurrency) as executor:
         executor.map(clone_or_pull_project, actions)
     elapsed = progress.finish_progress()
     log.debug("Syncing projects took [%s]", elapsed)
 
 
-def get_git_actions(root, dest, recursive, use_fetch, inline_token):
+def get_git_actions(root, dest, recursive, use_fetch, hide_token):
     actions = []
     for child in root.children:
         path = "%s%s" % (dest, child.root_path)
         if not os.path.exists(path):
             os.makedirs(path)
         if child.is_leaf:
-            actions.append(GitAction(child, path, recursive, use_fetch, inline_token))            
+            actions.append(GitAction(child, path, recursive, use_fetch, hide_token))            
         if not child.is_leaf:
-            actions.extend(get_git_actions(child, dest, recursive, use_fetch, inline_token))
+            actions.extend(get_git_actions(child, dest, recursive, use_fetch, hide_token))
     return actions
 
 
@@ -80,7 +80,7 @@ def clone_or_pull_project(action):
         if(action.recursive):
             multi_options.append('--recursive')
         if(action.use_fetch):
-            multi_options.append('--mirror')        
+            multi_options.append('--mirror')
         try:
             git.Repo.clone_from(action.node.url, action.path, multi_options=multi_options)
                 
