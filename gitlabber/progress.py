1c621e1cfc6fca8184c9d501a23950d1dbc9bc42
Erez <erez@dots.eco>
2024-07-02 20:39:24 +0700

    - Fix showing elapsed time as None when progress bar is disabled (verbose mode)   - Fixes https://github.com/ezbz/gitlabber/issues/96

--- gitlabber/progress.py
+++ gitlabber/progress.py
@@ -7,13 +7,12 @@ class ProgressBar:
         self.progress = None
         self.description = description
         self.disabled = disabled
-        self.start = None
+        self.start = time.time()
 
     def init_progress(self, total):
         if self.progress is None:
-            self.start = time.time()
             self.progress = tqdm(total=total, unit="projects",
-                                 bar_format="{desc}: {percentage:.1f}%|{bar:80}| {n_fmt}/{total_fmt}{postfix}", desc=self.description, leave=False, disable=self.disabled)
+                                 bar_format="{desc}: {percentage:.1f}%|{bar:80}| {n_fmt}/{total_fmt}{postfix}", desc=self.description, leave=False, disable=self.disabled)        
 
     def update_progress_length(self, added):
         if self.progress is not None:
@@ -29,7 +28,7 @@ class ProgressBar:
     def finish_progress(self):
         if self.progress is not None:
             self.progress.close()
-            end = time.time()
-            hours, rem = divmod(end-self.start, 3600)
-            minutes, seconds = divmod(rem, 60)
-            return "{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds)
+        end = time.time()   
+        hours, rem = divmod(end-self.start, 3600)
+        minutes, seconds = divmod(rem, 60)
+        return "{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds)
